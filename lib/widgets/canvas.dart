7b37c1cdb712685bc4cba054c1f7d8b5edbc08c2
nicole <nicolezhang83@icloud.com>
2021-03-27 17:04:26 -0400

    Added canvas

--- lib/widgets/canvas.dart
+++ lib/widgets/canvas.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class CanvasWidget extends StatefulWidget {
   @override
   _CanvasState createState() => _CanvasState();
 }
 
+class DrawingArea {
+  Offset point;
+  Paint areaPaint;
+
+  DrawingArea({this.point, this.areaPaint});
+}
+
 class _CanvasState extends State<CanvasWidget> {
+  List<DrawingArea> points = [];
+  Color selectedColor;
+  double strokeWidth;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedColor = Colors.black;
+    strokeWidth = 2.0;
+  }
+
+  void selectColor() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('Color Chooser'),
+        content: SingleChildScrollView(
+          child: BlockPicker(
+            pickerColor: selectedColor,
+            onColorChanged: (color) {
+              this.setState(() {
+                selectedColor = color;
+              });
+            },
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Close"))
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
@@ -42,13 +88,37 @@ class _CanvasState extends State<CanvasWidget> {
                           spreadRadius: 1.0)
                     ]),
                 child: GestureDetector(
-                  onPanDown: (details) {},
-                  onPanUpdate: (details) {},
-                  onPanEnd: (details) {},
+                  onPanDown: (details) {
+                    this.setState(() {
+                      points.add(DrawingArea(
+                          point: details.localPosition,
+                          areaPaint: Paint()
+                            ..strokeCap = StrokeCap.round
+                            ..isAntiAlias = true
+                            ..color = selectedColor
+                            ..strokeWidth = strokeWidth));
+                    });
+                  },
+                  onPanUpdate: (details) {
+                    this.setState(() {
+                      points.add(DrawingArea(
+                          point: details.localPosition,
+                          areaPaint: Paint()
+                            ..strokeCap = StrokeCap.round
+                            ..isAntiAlias = true
+                            ..color = selectedColor
+                            ..strokeWidth = strokeWidth));
+                    });
+                  },
+                  onPanEnd: (details) {
+                    this.setState(() {
+                      points.add(null);
+                    });
+                  },
                   child: ClipRRect(
                     borderRadius: BorderRadius.all(Radius.circular(20.0)),
                     child: CustomPaint(
-                      painter: MyCustomPainter(),
+                      painter: MyCustomPainter(points: points),
                     ),
                   ),
                 ),
@@ -64,8 +134,29 @@ class _CanvasState extends State<CanvasWidget> {
                   borderRadius: BorderRadius.all(Radius.circular(20.0)),
                 ),
                 child: Row(children: <Widget>[
-                  IconButton(icon: Icon(Icons.color_lens), onPressed: () {}),
-                  IconButton(icon: Icon(Icons.layers_clear), onPressed: () {}),
+                  IconButton(
+                      icon: Icon(Icons.color_lens, color: selectedColor),
+                      onPressed: () {
+                        selectColor();
+                      }),
+                  Expanded(
+                      child: Slider(
+                          min: 1.0,
+                          max: 7.0,
+                          activeColor: selectedColor,
+                          value: strokeWidth,
+                          onChanged: (value) {
+                            this.setState(() {
+                              strokeWidth = value;
+                            });
+                          })),
+                  IconButton(
+                      icon: Icon(Icons.layers_clear),
+                      onPressed: () {
+                        this.setState(() {
+                          points.clear();
+                        });
+                      }),
                 ]),
               ),
             ],
@@ -77,16 +168,28 @@ class _CanvasState extends State<CanvasWidget> {
 }
 
 class MyCustomPainter extends CustomPainter {
+  List<DrawingArea> points;
+  MyCustomPainter({this.points});
+
   @override
   void paint(Canvas canvas, Size size) {
     Paint background = Paint()..color = Colors.white;
     Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
     canvas.drawRect(rect, background);
-    canvas.clipRect(rect);
+
+    for (int x = 0; x < points.length - 1; x++) {
+      if (points[x] != null && points[x + 1] != null) {
+        Paint paint = points[x].areaPaint;
+        canvas.drawLine(points[x].point, points[x + 1].point, paint);
+      } else if (points[x] != null && points[x + 1] == null) {
+        Paint paint = points[x].areaPaint;
+        canvas.drawPoints(PointMode.points, [points[x].point], paint);
+      }
+    }
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    return null;
+    return true;
   }
 }
