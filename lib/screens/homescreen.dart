3fbcebe0c78279a32f0926ffeebf63801ff55a91
Anda Achimescu <anda.a@live.ca>
2021-03-27 20:16:26 -0400

    centred buttons

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -15,70 +15,77 @@ class _HomeState extends State<Home> {
       body: Stack(
         children: <Widget>[
           Container(
-          decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("lib/images/background.jpg"), fit: BoxFit.cover),
-        ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("lib/images/background.jpg"),
+                  fit: BoxFit.cover),
+            ),
+          ),
 
-        ),
           // Welcome photo
-          Padding(
-            padding: const EdgeInsets.only(top: 110.0, left:46.0),
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("lib/images/WelcomeWindowPlus.png"),
-                    fit: BoxFit.cover),
-              ),
-              height: 437,
-              width: 331,
-            ),
-          ),
+          Center(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(top: 110.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image:
+                                AssetImage("lib/images/WelcomeWindowPlus.png"),
+                            fit: BoxFit.cover),
+                      ),
+                      height: 437,
+                      width: 331,
+                    )),
 
-          // Join Room
-          Padding(
-            padding: const EdgeInsets.only(top: 583.0, left:46.0),
-            child: Container(
-              child: FloatingActionButton.extended(
-                backgroundColor: Color.fromARGB(255,233,118,97),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(38)),
-                label: Text("JOIN ROOM", style: TextStyle(fontSize: 20, fontFamily: 'Roboto')),
-                elevation: 10.0,
-                onPressed: (){
-                  print('join room has been pressed');
-                  Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => JoinRoom()));
-                }
+                // Join Room
+                Padding(
+                  padding: const EdgeInsets.only(top: 35.0),
+                  child: Container(
+                    child: FloatingActionButton.extended(
+                        backgroundColor: Color.fromARGB(255, 233, 118, 97),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(38)),
+                        label: Text("JOIN ROOM",
+                            style:
+                                TextStyle(fontSize: 20, fontFamily: 'Roboto')),
+                        elevation: 10.0,
+                        onPressed: () {
+                          print('join room has been pressed');
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => JoinRoom()));
+                        }),
+                    height: 63,
+                    width: 331,
+                  ),
                 ),
-              height: 63,
-              width: 331,
-            ),
-           
-          ),
-          
-          // Create Room
-          Padding(
-            padding: const EdgeInsets.only(top: 682.0, left:46.0),
-            child: Container(
-              child: FloatingActionButton.extended(
-                backgroundColor: Color.fromARGB(255,233,118,97),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(38)),
-                label: Text("CREATE ROOM", style: TextStyle(fontSize: 20, fontFamily: 'Roboto')),
-                elevation: 10.0,
-                onPressed: (){
-                  print('create room has been pressed');
-                  Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => CreateRoom()));
-                }
-                ),
-              height: 63,
-              width: 331,
-            ),
-          )
 
+                // Create Room
+                Padding(
+                  padding: const EdgeInsets.only(top: 35.0),
+                  child: Container(
+                    child: FloatingActionButton.extended(
+                        backgroundColor: Color.fromARGB(255, 233, 118, 97),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(38)),
+                        label: Text("CREATE ROOM",
+                            style:
+                                TextStyle(fontSize: 20, fontFamily: 'Roboto')),
+                        elevation: 10.0,
+                        onPressed: () {
+                          print('create room has been pressed');
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => CreateRoom()));
+                        }),
+                    height: 63,
+                    width: 331,
+                  ),
+                )
+              ]))
         ],
       ),
     );
   }
 }
-
