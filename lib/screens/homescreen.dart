ae9bf07c8fc2befbcffb89de954ad5c30742d2d7
nicole <nicolezhang83@icloud.com>
2021-03-27 20:51:25 -0400

    fixed confict issues

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -2,12 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hla/screens/create_room.dart';
 import 'package:hla/screens/join_room.dart';
-<<<<<<< HEAD
 import 'package:hla/services/roomDatabase.dart';
-=======
 import 'package:hla/screens/game_page.dart';
 import 'package:flutter/services.dart';
->>>>>>> dbeceb0ed167af4a1bb5269087c72388524ddc8c
 
 class Home extends StatefulWidget {
   @override
@@ -26,38 +23,6 @@ class _HomeState extends State<Home> {
   }
 
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return Scaffold(
-      body: Container(
-
-      // this is how we are structuring our elements on the screen
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('Join Room'),
-              onPressed: () {
-                print('join room has been pressed');
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => JoinRoom()));
-              },
-            ),
-            RaisedButton(
-              child: Text('Create Room'),
-              onPressed: () async {
-                // create instance of a new room
-                final RoomDatabaseService _roomdb = RoomDatabaseService();
-                // we need to keep track of the document id
-                // we wait until the db entry is done and ask for the id to confirm
-                String gameID = await _roomdb.getRoomID();
-                print('New room created. ID: $gameID');
-
-                print('create room has been pressed');
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => CreateRoom(gameID)));
-              },
-            )
-          ],
-        ),
-      )
-=======
     return Container(
       // Background
       decoration: BoxDecoration(
@@ -91,15 +56,21 @@ class _HomeState extends State<Home> {
           ),
           ElevatedButton(
             child: Text('Create Room'),
-            onPressed: () {
+            onPressed: () async {
+              // create instance of a new room
+              final RoomDatabaseService _roomdb = RoomDatabaseService();
+              // we need to keep track of the document id
+              // we wait until the db entry is done and ask for the id to confirm
+              String gameID = await _roomdb.getRoomID();
+              print('New room created. ID: $gameID');
+
               print('create room has been pressed');
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => CreateRoom()));
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => CreateRoom(gameID)));
             },
           )
         ],
       ),
->>>>>>> dbeceb0ed167af4a1bb5269087c72388524ddc8c
     );
   }
 }
